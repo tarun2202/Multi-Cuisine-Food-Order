57edcdac256264ebd3e83b433bcc584d48ca2233
tarun.2202 <kumartk.2202@gmail.com>
2024-02-15 19:21:11 +0530

    Unit Test: AdminTest added

---
+++ BackendJava/Prototype1/src/test/java/com/app/dao/AdminTest.java
@@ -0,0 +1,40 @@
+package com.app.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.app.entities.Admin;
+import com.app.entities.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AdminTest {
+
+    private Admin admin;
+
+    @BeforeEach
+    public void setUp() {
+        admin = new Admin();
+        admin.setAdminEmail("admin@example.com");
+        admin.setAdminPassword("password");
+        admin.setUserRole(UserRole.ROLE_ADMIN);
+    }
+
+    @Test
+    public void testAdminEmail() {
+        assertEquals("admin@example.com", admin.getAdminEmail());
+    }
+
+    @Test
+    public void testAdminPassword() {
+        assertEquals("password", admin.getAdminPassword());
+    }
+
+    @Test
+    public void testUserRole() {
+        assertEquals(UserRole.ROLE_ADMIN, admin.getUserRole());
+    }
+
+}
+
